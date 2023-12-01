1248f78551fae78a50dec402bd68094ee658844f
Alex Law <alex.law@global4.co.uk>
2023-12-01 15:19:15 +0000

    ✨ day 1

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +21,55 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('1a', function () {
-    $this->comment('Day 1a');
-})->purpose('Day 1a');
+    $input = Storage::disk('local')->get('1/a.txt');
+    $result = str($input)
+        ->trim()
+        ->explode(PHP_EOL)
+        ->map(function (string $line) {
+            return collect(str_split($line));
+        })
+        ->map(function (Collection $line) {
+            return $line->filter(fn ($char) => is_numeric($char));
+        })
+        ->map(function (Collection $line) {
+            return collect([$line->first(), $line->last()]);
+        })
+        ->map(fn (Collection $line) => $line->join(''))
+        ->map(fn (string $calibrationValue) => (int)$calibrationValue)
+        ->sum();
+
+    dd($result);
+});
+
+Artisan::command('1b', function () {
+    $input = Storage::disk('local')->get('1/a.txt');
+    $result = str($input)
+        ->trim()
+        ->explode(PHP_EOL)
+        ->map(function (string $line) {
+            return str($line)
+                ->replace('one', 'one1one')
+                ->replace('two', 'two2two')
+                ->replace('three', 'three3three')
+                ->replace('four', 'four4four')
+                ->replace('five', 'five5five')
+                ->replace('six', 'six6six')
+                ->replace('seven', 'seven7seven')
+                ->replace('eight', 'eight8eight')
+                ->replace('nine', 'nine9nine');
+        })
+        ->map(function (string $line) {
+            return collect(str_split($line));
+        })
+        ->map(function (Collection $line) {
+            return $line->filter(fn ($char) => is_numeric($char));
+        })
+        ->map(function (Collection $line) {
+            return collect([$line->first(), $line->last()]);
+        })
+        ->map(fn (Collection $line) => $line->join(''))
+        ->map(fn (string $calibrationValue) => (int)$calibrationValue)
+        ->sum();
+
+    dd($result);
+});
