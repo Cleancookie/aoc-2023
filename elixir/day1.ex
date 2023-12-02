9380edb69abaf4a9ae58550a77c7f3c449fc3a5d
Alex Law <pokemon999101@hotmail.co.uk>
2023-12-02 01:50:33 +0000

    🤔 HUH elixir??

--- elixir/day1.ex
+++ elixir/day1.ex
@@ -1,32 +1,30 @@
-input =
-  File.read!("../storage/app/1/a.txt")
-  |> String.trim()
-  |> String.split("\n")
-  |> Enum.map(fn line ->
-    replacements = %{
-      "one" => "one1one",
-      "two" => "two2two",
-      "three" => "three3three",
-      "four" => "four4four",
-      "five" => "five5five",
-      "six" => "six6six",
-      "seven" => "seven7seven",
-      "eight" => "eight8eight",
-      "nine" => "nine9nine"
-    }
+File.read!("../storage/app/1/a.txt")
+|> String.trim()
+|> String.split("\n")
+|> Enum.map(fn line ->
+  replacements = %{
+    "one" => "one1one",
+    "two" => "two2two",
+    "three" => "three3three",
+    "four" => "four4four",
+    "five" => "five5five",
+    "six" => "six6six",
+    "seven" => "seven7seven",
+    "eight" => "eight8eight",
+    "nine" => "nine9nine"
+  }
 
-    Enum.reduce(replacements, line, fn {key, value}, acc ->
-      String.replace(acc, key, value)
-    end)
+  Enum.reduce(replacements, line, fn {key, value}, acc ->
+    String.replace(acc, key, value)
   end)
-  |> Enum.map(&String.graphemes/1)
-  |> Enum.map(fn line ->
-    line
-    |> Enum.filter(&String.match?(&1, ~r/\d/))
-    |> Enum.chunk_every(1, 1, :discard)
-    |> List.first()
-    |> List.last()
-  end)
-  |> Enum.map(&String.to_integer/1)
-  |> Enum.sum()
-  |> IO.inspect(label: "Result")
+end)
+|> Enum.map(fn arg -> String.graphemes(arg) end)
+|> Enum.map(fn line ->
+  line
+  |> Enum.filter(&String.match?(&1, ~r/\d/)) # Filter out non-digits
+  |> Enum.chunk_every(1, 1, :discard) # Split into chunks of 1
+  |> fn digits ->  Enum.join([hd(digits), Enum.at(digits, -1)]) end.()
+end)
+|> Enum.map(&String.to_integer/1)
+|> Enum.sum()
+|> IO.inspect()
